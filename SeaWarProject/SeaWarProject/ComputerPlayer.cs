5e8730bdcfc075cbc16a2fbc1fa3a15bc209f929
Станислав Горожанкин <sgorozhankin2@gmail.com>
2018-02-06 09:25:47 +0200

    Добавлены классы игрок человек и игрок компьютер

---
+++ SeaWarProject/SeaWarProject/ComputerPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaWarProject
+{
+    class ComputerPlayer:Players
+    {
+        public ComputerPlayer()
+        {
+            InitShips();
+        }
+
+        public override void DoMove()
+        {
+
+        }
+    }
+}
---
+++ SeaWarProject/SeaWarProject/HumanPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaWarProject
+{
+    class HumanPlayer:Players
+    {
+        public HumanPlayer()
+        {
+            InitShips();
+        }
+
+        public override void DoMove()
+        {
+
+        }
+    }
+}
